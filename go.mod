64433e8cd02f0812b3e12f88c4cccf158c52a0a0
zoulongbo <zoulb@anchnet.com>
2021-05-29 22:26:52 +0800

    zookeeper

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/olivere/elastic/v7 v7.0.22
+	github.com/samuel/go-zookeeper v0.0.0-20201211165307-7117e9ea2414 // indirect
 	go.uber.org/zap v1.15.0 // indirect
 	golang.org/x/net v0.0.0-20201202161906-c7110b5ffcbb
 	golang.org/x/text v0.3.3
---
+++ zookeeper/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"github.com/samuel/go-zookeeper/zk"
+	"time"
+)
+
+var (
+	host = []string{"49.232.175.190:2181"}
+)
+
+func main()  {
+	conn, _, err := zk.Connect(host, 5 * time.Second)
+	if err != nil {
+		panic("conn err: " + err.Error())
+	}
+	defer conn.Close()
+	//新增
+	if _, err = conn.Create("/test_tree", []byte("tree content"), 0, zk.WorldACL(zk.PermAll)); err != nil {
+		fmt.Println("create err: ", err)
+		return
+	}
+	//查询
+	nodeV, dStat, err := conn.Get("/test_tree")
+	if err != nil {
+		fmt.Println("get err: ", err)
+		return
+	}
+	fmt.Println("nodeValue:", string(nodeV))
+
+	//改
+	if _, err = conn.Set("/test_tree", []byte("tree content1"), dStat.Version); err != nil {
+		fmt.Println("update err: ", err)
+		return
+	}
+	//删
+	_, dStat, _ = conn.Get("/test_tree")
+	if err = conn.Delete("/test_tree", dStat.Version); err != nil {
+		fmt.Println("delete err: ", err)
+		return
+	}
+	//验证
+	exists, _, err := conn.Exists("/test_tree")
+	if err != nil {
+		fmt.Println("exists err: ", err)
+		return
+	}
+	fmt.Println("exists:", exists)
+
+	//增加
+	if _, err = conn.Create("/test_tree1", []byte("tree content11"), 0, zk.WorldACL(zk.PermAll)); err != nil {
+		fmt.Println("once create err: ", err)
+
+	}
+	//设置子节点
+	if _, err = conn.Create("/test_tree1/sub", []byte("tree content11 - sub"), 0, zk.WorldACL(zk.PermAll)); err != nil {
+		fmt.Println("create sub err: ", err)
+
+	}
+	//获取子节点
+	children, _, err := conn.Children("/test_tree1")
+	if err != nil {
+		fmt.Println("children err: ", err)
+		return
+	}
+	fmt.Println("children: ", children)
+}
